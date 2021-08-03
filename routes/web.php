a859a9e7b6952df40fc3521ea5f6d6f5f6496e89
IRFANMUZAKKI <irfanmuzakki9c@gmail.com>
2021-08-03 12:13:38 +0700

    endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/me', [AuthController::class, 'index']);
